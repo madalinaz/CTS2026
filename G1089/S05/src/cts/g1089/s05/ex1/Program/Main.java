d47dca388713fabbdc334f1a14d8284645264ddd
Madalina Zurini <madalina.zurini@csie.ase.ro>
2026-03-20 08:33:15 +0200

    S05

--- G1089/S05/src/cts/g1089/s05/ex1/Program/Main.java
+++ G1089/S05/src/cts/g1089/s05/ex1/Program/Main.java
@@ -4,8 +4,7 @@ import cts.g1089.s05.ex1.Implementare.API.APIDimensiuni;
 
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         APIDimensiuni apiDimensiuni = APIDimensiuni.getInstance();
